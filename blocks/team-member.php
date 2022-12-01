4accd4632142553fe1a492ff47c1eb9877406330
Stephanie Ortiz <steph@imaginatii.com>
2022-11-30 22:12:19 -0500

    added team member block

---
+++ blocks/team-member.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * Testimonial Block Template.
+ *
+ * @param    array        $block      The block settings and attributes.
+ * @param    string       $content    The block inner HTML (empty).
+ * @param    bool         $is_preview True during AJAX preview.
+ * @param    (int|string) $post_id    The post ID this block is saved to.
+ */
+ 
+// Create id attribute allowing for custom "anchor" value.
+$id = 'testimonial-' . $block['id'];
+if( !empty($block['anchor']) ) {
+    $id = $block['anchor'];
+}
+
+// Create class attribute allowing for custom "className" and "align" values.
+$className = 'team-member';
+if( !empty($block['className']) ) {
+    $className .= ' ' . $block['className'];
+}
+if( !empty($block['align']) ) {
+    $className .= ' align' . $block['align'];
+}
+
+// Load values and assign defaults.
+$staffname             = get_field('staffname');
+$jobtitle             = get_field('jobtitle');
+$specialties           = get_field('specialties');
+$email             = get_field('email') ?: 'email contact';
+$staffimage            = get_field('staffimage') ?: 295;
+$linkedin = get_field('linkedin');
+$phone       = get_field('phone');
+$bio       = get_field('bio');
+
+
+?>
+
+
+
+
+<div id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>">
+ 
+<article itemscope itemtype ="https://schema.org/Person" class="teammember col-md-12">
+
+        <div class="col-xs-12 col-md-9">
+            
+             <h2 itemprop="name"><?php echo $staffname; ?></h2>
+
+             <h3 itemprop="JobTitle"><?php echo $jobtitle; ?></h3>
+
+            <?php 
+            $image = get_field('specialties');
+            if( !empty( $specialties ) ): ?>  
+              <h4> Specialties: <?php echo $specialties; ?></h4>
+            <?php endif; ?>
+
+             <h3 style="display: none;" itemprop="worksFor">Emerald Resource Group</h3>
+             <?php echo $bio; ?>&nbsp;
+
+             
+        </div>
+
+                <div class="col-xs-12 col-md-3 testimonial-image">
+            <?php 
+            $image = get_field('staffimage');
+            if( !empty( $staffimage ) ): ?>
+                <img itemprop="image" style="width: 100%" src="<?php echo esc_url($staffimage['url']); ?>" alt="<?php echo esc_attr($staffimage['alt']); ?>" />
+            <?php endif; ?>
+
+
+            <?php 
+            $image = get_field('linkedin');
+            if( !empty( $linkedin ) ): ?>       
+            <a class="btn btn-default" itemprop="sameAs" href="<?php echo $linkedin; ?>" target=_blank><span class="text"><span class="text-hold">LinkedIn</span><span class="icon icon-arrow-right"></span></span></a> 
+            <?php endif; ?>
+
+            <?php 
+            $image = get_field('email');
+            if( !empty( $email ) ): ?>  
+               <a class="btn btn-default" itemprop="email" href="mailto:<?php echo $email; ?>" target=_blank><span class="text">
+                <span class="text-hold">Email</span>
+              </span><span class="icon icon-arrow-right"></span></a> 
+              <?php endif; ?>
+
+            <?php 
+            $image = get_field('phone');
+            if( !empty( $phone ) ): ?>  
+             <a class="btn btn-default" itemprop="telephone" content="+<?php echo $phone; ?>" href="tel:+<?php echo $phone; ?>" target=_blank><span class="text"><span class="text-hold"> Phone</span><span class="icon icon-arrow-right"></span></span></a>
+             <?php endif; ?>
+            
+
+        </div>
+    
+    </article>
+    <style type="text/css">
+        #<?php echo $id; ?> {
+            background: <?php echo $background_color; ?>;
+            color: <?php echo $text_color; ?>;
+        }
+    </style>
+</div>
--- functions.php
+++ functions.php
@@ -5,7 +5,6 @@ if (file_exists(sys_get_temp_dir().'/staging-restrictions.php')) {
 	define('STAGING_RESTRICTIONS', true);
 	require_once sys_get_temp_dir().'/staging-restrictions.php';
 }
-
 include( get_template_directory() .'/classes.php' );
 include( get_template_directory() .'/widgets.php' );
 
@@ -21,6 +20,8 @@ function theme_disable_cheks() {
 }
 
 add_theme_support( 'automatic-feed-links' );
+add_theme_support( 'block-templates' );
+
 
 if ( ! isset( $content_width ) ) $content_width = 900;
 
@@ -1164,3 +1165,93 @@ $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_in
 return $instance;
 }
 } // Class wpb_widget ends here
+
+
+/**
+ * Shortcode Extended
+ *
+ * @package           ShortcodeExtended
+ * @author            Chris Sparshott
+ * @copyright         2022 Chris Sparshott
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Shortcode Extended
+ * Plugin URI:        https://github.com/seemly/shortcode-extended
+ * Description:       Enable ACF custom field usage in Query Loop block
+ * Version:           0.0.1
+ * Requires at least: 5.2
+ * Author:            Chris Sparshott
+ * Author URI:        https://sparshott.co.uk
+ * Text Domain:       shortcode-extended
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+if(!class_exists('ShortcodeExtended'))
+{
+  class ShortcodeExtended
+  {
+    public function render($attributes, $content, $data)
+    {
+      // just a failsafe
+      if(!($data instanceof WP_Block))
+      {
+        return $content;
+      }
+
+      // if no ACF not activated or installed, then return early.
+      if(!function_exists('get_field'))
+      {
+        return $content;
+      }
+
+      // if no ACF shortcode found in content, then return early.
+      if(strpos($content, '[acf ') === false)
+      {
+        return $content;
+      }
+
+      // Native functionality is to call `wpautop`, which means we lose access to the Post ID and ACF related data
+      return do_shortcode($content);
+    }
+  }
+
+  add_filter('register_block_type_args', function ($args, $name)
+  {
+    // Here we list the native blocks we are likely to include ACF shortcodes in.
+    // This list probably needs to be expanded, but suits my immediate requirements.
+    $validBlocks = ['core/shortcode', 'core/paragraph', 'core/list'];
+
+    // override the native render_callback function to ensure ACF shortcodes run as expected.
+    if(in_array($name, $validBlocks))
+    {
+      $args['render_callback'] = [new ShortcodeExtended, 'render'];
+    }
+
+    return $args;
+  }, 10, 2);
+}
+
+
+
+add_action('acf/init', 'my_acf_init_block_types');
+function my_acf_init_block_types() {
+    
+    // Check function exists.
+    if( function_exists('acf_register_block_type') ) {
+        
+        // register a team member block.
+        acf_register_block_type(array(
+            'name'              => 'teammember',
+            'title'             => __('Staff Member'),
+            'description'       => __('A custom team member block.'),
+            'render_template'   => 'blocks/team-member.php',
+            'category'          => 'formatting',
+            'icon'              => 'admin-comments',
+            'keywords'          => array( 'team member', 'staff' ),
+            'mode'              => 'preview',
+        ));
+    }
+
+}
